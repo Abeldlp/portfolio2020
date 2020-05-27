0b2ca9093f4f2e131b18bb6624bbb63c1e4c0bb3
Abel de la Paz <abel45991690@gmail.com>
2020-05-27 02:13:55 +0200

    Add backend server and mailing system to contact page

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/message", (req, res) => {
+  console.log(req.body);
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "abel45991690@gmail.com",
+    subject: req.body.subject,
+    html:
+      "<h3>From: " +
+      req.body.name +
+      "</h3></br><p>Email: " +
+      req.body.email +
+      "</p></br><p>Subject: " +
+      req.body.subject +
+      "</p></br><p>" +
+      req.body.message +
+      "</p>",
+  };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log("Error occurs: ", err);
+    } else {
+      console.log("Message sent");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
